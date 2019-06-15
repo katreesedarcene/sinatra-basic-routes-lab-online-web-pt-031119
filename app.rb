b6ff9c39e03b79cd2c8780c702dbfbdb4b810d7e
Kat B <katreesebarnes@gmail.com>
2019-06-15 16:32:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/name'
+  get '/name' do
+    "My name is Katreese"
+  end 
+  
+  get '/hometown' do
+    "My hometown is Enfield."
 end
